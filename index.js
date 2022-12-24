9ab486ceb779a03944eae2fd4984a6a3de1d607f
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-25 01:37:37 +0900

    feat: k번째 완전수 찾기 구현

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const fn = require("./fn");
 
-let i = 1 ;
-while(true){
-    if(fn.isPerfectNumber(i)){
-        console.log(i);
+let smallestPerfectNumber = 1;
+while (true) {
+    if (fn.isPerfectNumber(smallestPerfectNumber)) {
+        console.log('가장 작은 완전수 : ' + smallestPerfectNumber);
         break;
     }
-    i++;
+    smallestPerfectNumber++;
 }
+
+const k = 3 ; 
+const arr = [];
+let index = 1 ;
+console.log(`\n${k}번째 완전수 찾기`)
+while (arr.length < k) {
+    if (fn.isPerfectNumber(index)) {
+        console.log('찾은 완전수 : ' + index);
+        arr.push(index);
+    }
+    index++;
+}
+console.log(arr[arr.length-1]);
