5264aa9a72c0e7daa058cdb801ee1f94be468983
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-25 00:51:59 +0900

    refactor: 불필요한 함수 제거

--- fn.js
+++ fn.js
@@ -1,6 +1,5 @@
 const findDivisor = (num) => Array.from({length:num}, (v,i)=> i+1).filter((n)=>num%n===0);
-const sumOfDivisors = (arr) => arr.reduce((a,b)=>a+b,0);
 const fn = {
-    isPerfectNumber : (num) => num*2===sumOfDivisors(findDivisor(num)),
+    isPerfectNumber : (num) => num*2===findDivisor(num).reduce((a,b)=>a+b,0),
 }
 module.exports = fn;
