85463d95cad6f75a66bab05ae2f1854408317150
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-25 00:35:38 +0900

    refactor: 함수 단순화

--- fn.js
+++ fn.js
@@ -1,7 +1,4 @@
-const findDivisor = (num) => {
-    const arr = Array.from({length:num}, (v,i)=> i+1).filter((n)=>num%n===0);
-    return arr;
-};
+const findDivisor = (num) => Array.from({length:num}, (v,i)=> i+1).filter((n)=>num%n===0);
 const sumOfDivisors = (arr) => arr.reduce((a,b)=>a+b,0);
 const fn = {
     isPerfectNumber : (num) => num*2===sumOfDivisors(findDivisor(num)),
