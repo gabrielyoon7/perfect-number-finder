de3551c2a1c5ef080db18cba8252d64a52f28ece
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-25 13:43:28 +0900

    test: 테스트 케이스 추가

--- fn.test.js
+++ fn.test.js
@@ -1,14 +1,34 @@
 const fn = require("./fn");
-
-test('121', () => {
-    expect(fn.isPerfectNumber(121)).toBeFalsy();
+describe('1번 문제', () => {
+    test('가장 작은 완전수', () => {
+        expect(fn.findSmallestPerfectNumber()).toBe(6);
+    });
 });
-test('486', () => {
-    expect(fn.isPerfectNumber(486)).toBeFalsy();
+describe('2번 문제', () => {
+    test('121', () => {
+        expect(fn.isPerfectNumber(121)).toBeFalsy();
+    });
+    test('486', () => {
+        expect(fn.isPerfectNumber(486)).toBeFalsy();
+    });
+    test('496', () => {
+        expect(fn.isPerfectNumber(496)).toBeTruthy();
+    });
+    test('512', () => {
+        expect(fn.isPerfectNumber(512)).toBeFalsy();
+    });
 });
-test('496', () => {
-    expect(fn.isPerfectNumber(496)).toBeTruthy();
-});
-test('512', () => {
-    expect(fn.isPerfectNumber(512)).toBeFalsy();
+describe('3번 문제', () => {
+    test('1번째 완전수', () => {
+        expect(fn.findPerfectNumberOfK(1)).toBe(6);
+    });
+    test('2번째 완전수', () => {
+        expect(fn.findPerfectNumberOfK(2)).toBe(28);
+    });
+    test('3번째 완전수', () => {
+        expect(fn.findPerfectNumberOfK(3)).toBe(496);
+    });
+    test('4번째 완전수', () => {
+        expect(fn.findPerfectNumberOfK(4)).toBe(8128);
+    });
 });
