fba2cb6f1cc4b647e7900e0c832edebda1b94ebd
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-25 13:38:42 +0900

    refactor: 모듈화

--- fn.js
+++ fn.js
@@ -1,5 +1,30 @@
-const findDivisor = (num) => Array.from({length:num}, (v,i)=> i+1).filter((n)=>num%n===0);
-const fn = {
-    isPerfectNumber : (num) => num*2===findDivisor(num).reduce((a,b)=>a+b,0),
+const findDivisor = (num) => Array.from({ length: num }, (v, i) => i + 1).filter((n) => num % n === 0);
+const isPerfectNumber = (num) => num * 2 === findDivisor(num).reduce((a, b) => a + b, 0);
+const findSmallestPerfectNumber = () => {
+    let smallestPerfectNumber = 1;
+    while (true) {
+        if (isPerfectNumber(smallestPerfectNumber)) {
+            console.log('가장 작은 완전수 : ' + smallestPerfectNumber);
+            return smallestPerfectNumber;
+        }
+        smallestPerfectNumber++;
+    }
 }
-module.exports = fn;
+
+const findPerfectNumberOfK = (k) => {
+    const arr = [];
+    let index = 1;
+    console.log(`\n${k}번째 완전수 찾기`)
+    while (arr.length < k) {
+        if (isPerfectNumber(index)) {
+            console.log('찾은 완전수 : ' + index);
+            arr.push(index);
+        }
+        index++;
+    }
+    const perfectNumberOfK = arr[arr.length - 1];
+    console.log(perfectNumberOfK);
+    return perfectNumberOfK;
+}
+
+module.exports = { findDivisor, isPerfectNumber, findSmallestPerfectNumber, findPerfectNumberOfK };
--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 const fn = require("./fn");
 
-let smallestPerfectNumber = 1;
-while (true) {
-    if (fn.isPerfectNumber(smallestPerfectNumber)) {
-        console.log('가장 작은 완전수 : ' + smallestPerfectNumber);
-        break;
-    }
-    smallestPerfectNumber++;
-}
+fn.findSmallestPerfectNumber();
 
-const k = 3 ; 
-const arr = [];
-let index = 1 ;
-console.log(`\n${k}번째 완전수 찾기`)
-while (arr.length < k) {
-    if (fn.isPerfectNumber(index)) {
-        console.log('찾은 완전수 : ' + index);
-        arr.push(index);
-    }
-    index++;
-}
-console.log(arr[arr.length-1]);
+fn.findPerfectNumberOfK(3);
